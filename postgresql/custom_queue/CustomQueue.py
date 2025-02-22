5c6e046af62c00c4c31694332f21e4aa34161441
gngsn <enfj.dev@gmail.com>
2025-02-22 13:21:09 +0900

    pg: add operation methods to custom queue

--- postgresql/custom_queue/CustomQueue.py
+++ postgresql/custom_queue/CustomQueue.py
@@ -1,6 +1,8 @@
 import json
+import random
+from uuid import uuid4
 
-from peewee import IntegerField, CharField, DateTimeField, CompositeKey, BigIntegerField
+from peewee import IntegerField, CharField, DateField, DateTimeField, CompositeKey, BigIntegerField
 from pendulum import datetime, now, date
 
 from peewee import *
@@ -9,75 +11,93 @@ from playhouse.postgres_ext import JSONField
 NOTIFICATION_QUEUE_TABLE_NAME = 'custom_queue'
 PARTITION_FORMAT = "YYYY_MM_DD"
 
-pg_db = PostgresqlDatabase('test', user='postgres', password='postgres', host='localhost', port=5432)
+database_ = PostgresqlDatabase('postgres', user='postgres', password='postgres', host='localhost', port=5332)
+
 
 class BaseModel(Model):
     class Meta:
-        database = pg_db
+        database = database_
 
 
 class CustomQueue(BaseModel):
     """ Notification Queue DB Model """
 
-    id: int = BigIntegerField()
+    name: str = CharField(max_length=30)
+    key: str = CharField(max_length=256)
     status: int = IntegerField(default=0, null=False)
-    try_count: int = IntegerField(default=0, null=False)
-    max_tries: int = IntegerField(default=3, null=False)
-    key: str = CharField(max_length=200)
-    payload: int = JSONField(default='{}')
+    payload: json = JSONField(default='{}')
 
+    try_count: int = IntegerField(default=0, null=False)
+
+    enqueue_dt: date = DateField(default=now())
     created_at: datetime = DateTimeField(default=now())
     updated_at: datetime = DateTimeField(default=now())
-    priority: int = IntegerField(default=0, null=False)
 
     class Meta:
         table_name = NOTIFICATION_QUEUE_TABLE_NAME
-        primary_key = CompositeKey('id', 'created_at')
+        primary_key = CompositeKey('name', 'key', 'enqueue_dt')
         indexes = (
-            (('checksum', 'created_at'), True),
+            (('status', 'created_at'), True),
         )
-        table_settings = 'PARTITION BY RANGE (created_at)'
+        table_settings = 'PARTITION BY RANGE (enqueue_dt)'
 
     @classmethod
-    def enqueue(cls, key: str, message: dict):
-        cls.insert(key=key, payload=json.dumps(message)).execute()
+    def enqueue(cls, name: str, key: str, message: dict):
+        cls.insert(name=name, key=key, payload=json.dumps(message)).execute()
 
     @classmethod
     def dequeue(cls, offset: int, limit: int):
         return cls.select().where(cls.status == 0).offset(offset).limit(limit).for_update("FOR UPDATE SKIP LOCKED")
 
     @classmethod
-    def update_success_done(cls, id):
+    def update_success_done(cls, name, key):
         return cls.update(
-            try_account=cls.try_count + 1,
+            try_count=cls.try_count + 1,
             status=1,
             updated_at=now()
-        ).where(cls.id == id).execute()
+        ).where(cls.name == name, cls.key == key).execute()
 
     @classmethod
-    def update_failure_done(cls, id):
+    def update_failure_done(cls, name: str, key: str):
         where = cls.update(
-            try_account=cls.try_count + 1,
+            try_count=cls.try_count + 1,
             status=-1,
             updated_at=now()
-        ).where(cls.id == id)
+        ).where(cls.name == name, cls.key == key)
         return where.execute()
 
-if __name__ == "__main__":
-    now = datetime(year=2024, month=5, day=23, hour=13, minute=13)
-    CustomQueue.enqueue(
-        "UMSV10001:12",
-        {"message": "hello world", "user_id": 12},
-    )
 
-    chunk_size = 100
-    offset = 0
-    limit = chunk_size
-    dequeue = CustomQueue.dequeue(offset, limit)
+def execute(sql):
+    return database_.execute_sql(sql)
 
-    print(dequeue)
-    for message in dequeue:
-        print(message)
 
-if __name__ == "__main__":
-    CustomQueue.create_partition_if_not_exists(date(year=2024, month=5, day=23))
+def is_exist_partition(table_name, partition_name):
+    cursor = execute(f"""
+            SELECT
+                parent.relname      AS parent,
+                child.relname       AS child
+            FROM pg_inherits
+                JOIN pg_class parent            ON pg_inherits.inhparent = parent.oid
+                JOIN pg_class child             ON pg_inherits.inhrelid   = child.oid
+            WHERE parent.relname='{table_name}' and child.relname='{partition_name}';
+        """)
+
+    return cursor.rowcount != 0
+
+
+def create_partition_if_not_exists(now_date=now().date()):
+    def get_partition_name():
+        formatted_date = now_date.format(PARTITION_FORMAT)
+
+        return f"{NOTIFICATION_QUEUE_TABLE_NAME}_{format(formatted_date)}"
+
+    table_name = NOTIFICATION_QUEUE_TABLE_NAME
+    partition_name = get_partition_name()
+
+    if is_exist_partition(table_name, partition_name):
+        return
+
+    execute(f"""
+            CREATE TABLE {partition_name}
+            PARTITION OF {table_name}
+            FOR VALUES FROM ('{now_date}') TO ('{now_date.add(days=1)}')""")
